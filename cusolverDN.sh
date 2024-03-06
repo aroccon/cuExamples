a1dcee35552bc810fd4945b9edb0a61747e27cca
aroccon <alessio.roccon@uniud.it>
2024-03-06 17:52:03 +0100

    new_arrangement3

--- cusolverDN.sh
+++
@@ -1,9 +0,0 @@
-#update according to the nvhpc release available
-NVARCH=Linux_x86_64; export NVARCH
-NVCOMPILERS=/opt/nvidia/hpc_sdk; export NVCOMPILERS
-MANPATH=$MANPATH:$NVCOMPILERS/$NVARCH/23.7/compilers/man; export MANPATH
-PATH=$NVCOMPILERS/$NVARCH/23.7/compilers/bin:$PATH; export PATH
-export PATH=$NVCOMPILERS/$NVARCH/23.7/comm_libs/mpi/bin:$PATH
-export MANPATH=$MANPATH:$NVCOMPILERS/$NVARCH/23.7/comm_libs/mpi/man
-nvfortran -Minfo=accel  -gpu=managed  -acc -cuda  -cudalib=cublas,cusolver  main.f90 -o cusolver
-./cusolver
